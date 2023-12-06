1e4fde51e1e015bb9ca1818656a7bf7feea5ecd0
Darek Mazur <dev@darekmazur.it>
2023-12-06 04:10:11 +0100

    Add theme with theme provider

---
+++ src/utils/themes/theme.ts
@@ -0,0 +1,37 @@
+export const theme = {
+  colors: {
+    white: '#EFEFEF',
+    dark: '#444',
+    brown: '#93806F',
+    green: '#74917D',
+    lightGreen: '#79C791',
+    red: '#C70039',
+  },
+
+  fonts: {
+    main: '"Montserrat", sans-serif',
+  },
+
+  fontSize: {
+    xl: '4rem',
+    l: '3rem',
+    lm: '1.8rem',
+    m: '1.6rem',
+    s: '1.4rem',
+    xs: '1.2rem',
+  },
+
+  fontWeight: {
+    regular: '400',
+    bold: '600',
+    fat: '900',
+    thin: '200',
+  },
+
+  breakpoints: {
+    wide: '1440',
+    desktop: '1080',
+    tablet: '800',
+    mobile: '400',
+  },
+};
