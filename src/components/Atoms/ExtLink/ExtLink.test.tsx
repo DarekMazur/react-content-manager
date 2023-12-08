6a629817988b61520e1e3625e0cd1ffa70ab457d
Darek Mazur <dev@darekmazur.it>
2023-12-08 06:00:29 +0100

    Test external link componenet

---
+++ src/components/Atoms/ExtLink/ExtLink.test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import { renderWithProvider } from '../../../utils/helpers/renderWithProvider.tsx';
+import ExtLink from './ExtLink.tsx';
+
+const customLink = renderWithProvider(
+  <ExtLink url="https://google.com" name="My Link" />,
+);
+
+describe('Render external link:', () => {
+  it('- link contains custom label', () => {
+    expect(screen.getByText('My Link'));
+  });
+
+  it('- link contains url attribute', () => {
+    renderWithProvider(<ExtLink url="https://google.com" name="My Link" />);
+    expect(screen.getByText('My Link')).toHaveAttribute(
+      'href',
+      'https://google.com',
+    );
+  });
+
+  it('- link has default size', () => {
+    renderWithProvider(<ExtLink url="https://google.com" name="My Link" />);
+    expect(screen.getByText('My Link')).toHaveStyle('font-size: inherit');
+  });
+
+  it('- link has proper set size', () => {
+    renderWithProvider(
+      <ExtLink url="https://google.com" name="My Link" size="l" />,
+    );
+    expect(screen.getByText('My Link')).toHaveStyle('font-size: 48px');
+  });
+
+  it('- rendered component match to snapshot', () => {
+    expect(customLink).toMatchSnapshot();
+  });
+});
