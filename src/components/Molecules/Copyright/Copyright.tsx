0e5fc78c2db0d06c5b6a496e0100129d4278281b
Darek Mazur <dev@darekmazur.it>
2023-12-08 06:37:39 +0100

    Refactor get year method

--- src/components/Molecules/Copyright/Copyright.tsx
+++ src/components/Molecules/Copyright/Copyright.tsx
@@ -1,12 +1,12 @@
 import Link from '../../Atoms/ExtLink/ExtLink.tsx';
 import { StyledCopyright } from './Copyright.styles.ts';
 import { data } from '../../../utils/data.ts';
-import { getDate } from '../../../utils/methods/getDate.ts';
+import { getYear } from '../../../utils/methods/getYear.ts';
 
 const Copyright = () => {
   return (
-    <StyledCopyright>
-      {getDate()} &copy; <Link url={data.authorUrl} name={data.authorName} />
+    <StyledCopyright role="copyright">
+      {getYear()} &copy; <Link url={data.authorUrl} name={data.authorName} />
     </StyledCopyright>
   );
 };
