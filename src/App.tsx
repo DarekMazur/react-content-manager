9df0ec2ee41f5a21849f3513b0ee8c93f3f624aa
Darek Mazur <dev@darekmazur.it>
2023-12-08 14:07:36 +0100

    Create mock admin user

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './utils/themes/theme.ts';
 import FooterWrapper from './components/Organisms/Footer/Footer.tsx';
 import Header from './components/Organisms/Header/Header.tsx';
+import { adminUser } from './utils/data.ts';
 
 const App = () => {
+  const user = {
+    userName: adminUser.username,
+    avatar: adminUser.avatar,
+    uuid: adminUser.uuid,
+  };
+
+  const isAuthorised = () => {
+    return adminUser.role.type === 'admin';
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header isAuthorised={true} />
+      <Header user={user} isAuthorised={isAuthorised()} />
       <div>
         <p>Clear project</p>
       </div>
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 interface dataTypes {
   blogName: string;
   blogUrl: string;
@@ -10,4 +12,25 @@ export const data: dataTypes = {
   blogUrl: 'https://lesnygacek.pl',
   authorName: 'Nerdistry',
   authorUrl: 'https://nerdistry.pl',
+};
+
+export const adminUser = {
+  id: 1,
+  username: faker.person.fullName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar(),
+  provider: 'local',
+  confirmed: true,
+  blocked: false,
+  createdAt: '2023-12-08T11:17:05.804Z',
+  updatedAt: '2023-12-08T11:17:05.804Z',
+  uuid: faker.string.uuid(),
+  role: {
+    id: 1,
+    name: 'Administrator',
+    description: 'Page admin',
+    type: 'admin',
+    createdAt: '2023-06-18T21:11:04.852Z',
+    updatedAt: '2023-06-18T21:11:04.852Z',
+  },
 };
