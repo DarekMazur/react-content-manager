464293a9a174510caae64f88696eb907b6581de6
Darek Mazur <dev@darekmazur.it>
2023-12-09 10:40:45 +0100

    Test icon

--- src/components/Atoms/Icon/Icon.tsx
+++ src/components/Atoms/Icon/Icon.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { StyledIcon } from './Icon.styles.ts';
+import { adminUser } from '../../../utils/data.ts';
 
 interface IconProps {
   customIcon?: string;
@@ -7,12 +8,17 @@ interface IconProps {
 
 const Icon = (props: IconProps) => {
   const { customIcon } = props;
+  const imageUrlRegex = /\.(jpeg|jpg|gif|png|svg)$/i;
+
   return (
     <StyledIcon>
-      {customIcon ? (
-        <img src={customIcon} alt="" />
+      {customIcon && imageUrlRegex.test(customIcon) ? (
+        <img src={customIcon} alt={`${adminUser.username} avatar`} />
       ) : (
-        <FontAwesomeIcon icon={['fas', 'user']} />
+        <FontAwesomeIcon
+          icon={['fas', 'user']}
+          aria-label={`${adminUser.username}`}
+        />
       )}
     </StyledIcon>
   );
