36d5c641fd9830b3e4df9802f994cb7417c70d5d
Darek Mazur <dev@darekmazur.it>
2023-12-10 14:16:58 +0100

    Add router to main component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { theme } from './utils/themes/theme.ts';
 import FooterWrapper from './components/Organisms/Footer/Footer.tsx';
 import Header from './components/Organisms/Header/Header.tsx';
 import { adminUser } from './utils/data.ts';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   const user = {
@@ -22,9 +22,62 @@ const App = () => {
       <BrowserRouter>
         <GlobalStyle />
         <Header user={user} isAuthorised={isAuthorised()} />
-        <div>
-          <p>Clear project</p>
-        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuthorised() ? (
+                <div>
+                  <p>Dashboard - authorised</p>
+                </div>
+              ) : (
+                <div>
+                  <p>Dashboard - unauthorised</p>
+                </div>
+              )
+            }
+          />
+          <Route
+            path="articles"
+            element={
+              <div>
+                <p>Articles</p>
+              </div>
+            }
+          />
+          <Route
+            path="comments"
+            element={
+              <div>
+                <p>Comments</p>
+              </div>
+            }
+          />
+          <Route
+            path="users"
+            element={
+              <div>
+                <p>Users</p>
+              </div>
+            }
+          />
+          <Route
+            path="user"
+            element={
+              <div>
+                <p>Single user</p>
+              </div>
+            }
+          />
+          <Route
+            path="gallery"
+            element={
+              <div>
+                <p>Gallery</p>
+              </div>
+            }
+          />
+        </Routes>
         <FooterWrapper />
       </BrowserRouter>
     </ThemeProvider>
