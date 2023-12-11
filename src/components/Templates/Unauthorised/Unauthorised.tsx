8a54b5d2640eac1d955428b53092662b7f5e415c
Darek Mazur <dev@darekmazur.it>
2023-12-11 06:38:50 +0100

    Create internal link component based on react router Link

--- src/components/Templates/Unauthorised/Unauthorised.tsx
+++ src/components/Templates/Unauthorised/Unauthorised.tsx
@@ -1,15 +1,25 @@
 import { FC, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { theme } from '../../../utils/themes/theme.ts';
-import { Link } from 'react-router-dom';
 import ExtLink from '../../Atoms/ExtLink/ExtLink.tsx';
 import { data } from '../../../utils/data.ts';
 import { StyledUnauthorised } from './Unauthorised.styles.ts';
 import { getFooterHeight } from '../../../utils/methods/getFooterHeight.ts';
+import styled from 'styled-components';
+import InLink from '../../Atoms/InLink/InLink.tsx';
 
 interface AuthProps {
   handleMockLogin: () => void;
 }
+
+const StyledLockIcon = styled.svg`
+  font-size: 14rem;
+  color: ${({ theme }) => theme.colors.darkBlue};
+`;
+
+const LockIcon = () => {
+  return <StyledLockIcon as={FontAwesomeIcon} icon={['fas', 'lock']} />;
+};
 
 const Unauthorised: FC<AuthProps> = ({ handleMockLogin }) => {
   const [wrapperHeight, setWrapperHeight] = useState(0);
@@ -20,23 +30,10 @@ const Unauthorised: FC<AuthProps> = ({ handleMockLogin }) => {
 
   return (
     <StyledUnauthorised $height={wrapperHeight}>
-      <FontAwesomeIcon
-        icon={['fas', 'lock']}
-        style={{ fontSize: '14rem', color: theme.colors.darkBlue }}
-      />
+      <LockIcon />
       <p style={{ fontSize: theme.fontSize.xl }}>
         You need to{' '}
-        <Link
-          style={{
-            color: theme.colors.blue,
-            textDecoration: 'none',
-          }}
-          onClick={handleMockLogin}
-          to="/"
-        >
-          log in
-        </Link>{' '}
-        first
+        <InLink target="/" name="log in" onClick={handleMockLogin} /> first
       </p>
       <p>or</p>
       <p>
