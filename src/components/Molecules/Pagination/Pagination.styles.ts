746639f7bbf0b14df22ce2ddf7ff3ec481517c89
Darek Mazur <dev@darekmazur.it>
2023-12-17 05:53:16 +0100

    Add redux to project

--- src/components/Molecules/Pagination/Pagination.styles.ts
+++ src/components/Molecules/Pagination/Pagination.styles.ts
@@ -10,13 +10,16 @@ export const StyledPagination = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
-  span {
-    padding: 0 0.5rem;
-  }
 `;
 
 export const StyledPaginationNumber = styled.span<StyledPaginationNumberProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.3rem;
+  border-radius: 0.3rem;
+  height: 3rem;
+  width: 3rem;
   padding: 0 0.5rem;
   font-weight: ${({ $number, $current, theme }) =>
     $number === $current ? theme.fontWeight.bold : 'inherit'};
@@ -24,4 +27,10 @@ export const StyledPaginationNumber = styled.span<StyledPaginationNumberProps>`
     $number === $current ? theme.colors.red : 'inherit'};
   cursor: ${({ $number, $current }) =>
     $number === $current ? 'auto' : 'pointer'};
+  transition: 200ms background-color ease-in-out;
+
+  &:hover {
+    background-color: ${({ theme, $current, $number }) =>
+      $current !== $number && theme.colors.lightBlue};
+  }
 `;
