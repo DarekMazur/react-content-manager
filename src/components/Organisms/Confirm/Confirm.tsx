79247a096e3318c4e7b4809402ce0bd683edd3a9
Darek Mazur <dev@darekmazur.it>
2024-01-03 03:41:28 +0100

    Add base styles to popup

--- src/components/Organisms/Confirm/Confirm.tsx
+++ src/components/Organisms/Confirm/Confirm.tsx
@@ -1,8 +1,35 @@
+import styled from 'styled-components';
+
+export const PopupWrapepr = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: ${({ theme }) => theme.colors.darkBluePopup};
+  z-index: 10;
+
+  div {
+    padding: 4rem 2rem;
+    background-color: ${({ theme }) => theme.colors.white};
+    border-radius: 5px;
+    width: 30%;
+    position: relative;
+  }
+`;
+
 const Confirm = () => {
   return (
-    <>
-      <p>Are you sure you? This is permanent</p>
-    </>
+    <PopupWrapepr>
+      <div>
+        <p>Are you sure you? This is permanent</p>
+        <button>delete</button>
+        <button>cancel</button>
+      </div>
+    </PopupWrapepr>
   );
 };
 
