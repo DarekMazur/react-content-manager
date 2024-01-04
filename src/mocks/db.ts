c300e7d2521d2772aa1f2c2c4e20e88c1804d551
Darek Mazur <dev@darekmazur.it>
2024-01-04 02:50:00 +0100

    Move roles to database

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -1,8 +1,41 @@
 import { faker } from '@faker-js/faker';
 import { factory, oneOf, primaryKey, nullable } from '@mswjs/data';
-import { roles } from './data/roles';
 
 faker.seed(123);
+
+export interface RoleTypes {
+  id: number;
+  name: string;
+  description: string;
+  type: string;
+}
+
+export const roles: Array<RoleTypes> = [
+  {
+    id: 1,
+    name: 'Administrator',
+    description: 'Page admin',
+    type: 'admin',
+  },
+  {
+    id: 2,
+    name: 'Redactor',
+    description: 'Blog redactor',
+    type: 'redactor',
+  },
+  {
+    id: 3,
+    name: 'Creator',
+    description: 'Content creator',
+    type: 'creator',
+  },
+  {
+    id: 4,
+    name: 'Authenticated',
+    description: 'User',
+    type: 'authenticated',
+  },
+];
 
 export const db = factory({
   user: {
