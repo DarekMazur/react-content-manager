52815b74b92308553a87feeab747b812a4efd699
Darek Mazur <dev@darekmazur.it>
2024-01-08 02:48:34 +0100

    Fix issue with pagination logic

--- src/components/Organisms/TableWrapper/TableWrapper.tsx
+++ src/components/Organisms/TableWrapper/TableWrapper.tsx
@@ -9,18 +9,19 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ArticleDataTypes } from '../../../types/dataTypes';
 
-const TableWrapper = ({
-  articles,
-}: {
-  articles: ArticleDataTypes[] | undefined;
-}) => {
+const TableWrapper = ({ articles }: { articles: ArticleDataTypes[] }) => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const [isExpand, setIsExpand] = useState(false);
   const [perPage, setPerPage] = useState(10);
-  const [pagesLength, setPagesLength] = useState(0);
-  const [pages, setPages] = useState(0);
-  const [postsToDisplay, setPostsToDisplay] = useState<ArticleDataTypes[]>([]);
+
+  const getPagesLength = Math.ceil(articles.length / perPage);
+  const [pages, setPages] = useState(getPagesLength);
+
+  const postsToDisplay = articles.slice(
+    (Number(searchParams.get('page')) - 1) * perPage,
+    perPage * Number(searchParams.get('page')),
+  );
 
   useEffect(() => {
     if (!searchParams.get('page')) {
@@ -33,19 +34,6 @@ const TableWrapper = ({
   }, []);
 
   useEffect(() => {
-    if (articles) {
-      setPagesLength(Math.ceil(articles.length / perPage));
-      setPostsToDisplay(
-        articles.slice(
-          (Number(searchParams.get('page')) - 1) * perPage,
-          perPage * Number(searchParams.get('page')),
-        ),
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [articles]);
-
-  useEffect(() => {
     if (postsToDisplay.length === 0 && Number(searchParams.get('page')) !== 1) {
       setSearchParams((params) => {
         params.set('page', String(Number(searchParams.get('page')) - 1));
@@ -56,8 +44,8 @@ const TableWrapper = ({
   }, [postsToDisplay]);
 
   useEffect(() => {
-    setPages(pagesLength);
-  }, [pagesLength]);
+    setPages(getPagesLength);
+  }, [getPagesLength]);
 
   const handleExpand = () => {
     setIsExpand((prevState) => !prevState);
@@ -81,46 +69,40 @@ const TableWrapper = ({
 
   return (
     <>
-      {articles && articles.length > 0 ? (
-        <>
-          <Wrapper width="100%" justify="center" align="flex-start" padding="0">
-            <Table headers={articlesTableHeaders} data={postsToDisplay} />
-          </Wrapper>
-          <Wrapper justify="space-between" align="center" width="97.5vw">
-            <EntriesNumberPicker
-              perPage={perPage}
-              isExpand={isExpand}
-              handleExpand={handleExpand}
-              handleChoseEntriesNumber={handleChoseEntriesNumber}
-              handleClose={handleClose}
-            />
-            <StyledReactPaginate>
-              <ReactPaginate
-                previousLabel={
-                  <FontAwesomeIcon icon={['fas', 'chevron-left']} />
-                }
-                nextLabel={<FontAwesomeIcon icon={['fas', 'chevron-right']} />}
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={1}
-                marginPagesDisplayed={1}
-                pageCount={pages}
-                previousClassName="page-nav"
-                previousLinkClassName="page-link"
-                nextClassName="page-nav"
-                nextLinkClassName="page-link"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                breakLabel="..."
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="pagination"
-                activeClassName="active"
-                renderOnZeroPageCount={null}
-              />
-            </StyledReactPaginate>
-          </Wrapper>
-        </>
-      ) : null}
+      <Wrapper width="100%" justify="center" align="flex-start" padding="0">
+        <Table headers={articlesTableHeaders} data={postsToDisplay} />
+      </Wrapper>
+      <Wrapper justify="space-between" align="center" width="97.5vw">
+        <EntriesNumberPicker
+          perPage={perPage}
+          isExpand={isExpand}
+          handleExpand={handleExpand}
+          handleChoseEntriesNumber={handleChoseEntriesNumber}
+          handleClose={handleClose}
+        />
+        <StyledReactPaginate>
+          <ReactPaginate
+            previousLabel={<FontAwesomeIcon icon={['fas', 'chevron-left']} />}
+            nextLabel={<FontAwesomeIcon icon={['fas', 'chevron-right']} />}
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={1}
+            marginPagesDisplayed={1}
+            pageCount={pages}
+            previousClassName="page-nav"
+            previousLinkClassName="page-link"
+            nextClassName="page-nav"
+            nextLinkClassName="page-link"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+          />
+        </StyledReactPaginate>
+      </Wrapper>
     </>
   );
 };
--- src/pages/Articles/Articles.tsx
+++ src/pages/Articles/Articles.tsx
@@ -8,17 +8,15 @@ import { ArticleDataTypes } from '../../types/dataTypes.ts';
 import TableWrapper from '../../components/Organisms/TableWrapper/TableWrapper.tsx';
 
 const Articles = () => {
-  const { data } = useGetArticlesQuery();
+  const { data: articles = [] } = useGetArticlesQuery();
 
   const selectedArticles = useSelector<RootState>((state) => state.selected);
 
   const [wrapperHeight, setWrapperHeight] = useState(0);
-  const [articles, setArticles] = useState<ArticleDataTypes[] | undefined>([]);
 
   useEffect(() => {
-    setArticles(data);
     setWrapperHeight(getFooterHeight() + 50);
-  }, [data]);
+  }, []);
 
   return (
     <main
