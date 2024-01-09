b255454754cb8007cfbe77d1c2bbc91ffa41b95a
Darek Mazur <dev@darekmazur.it>
2024-01-09 19:49:06 +0100

    Add logout function

--- src/components/Organisms/Header/Header.tsx
+++ src/components/Organisms/Header/Header.tsx
@@ -3,21 +3,55 @@ import Icon from '../../Atoms/Icon/Icon.tsx';
 import MenuList from '../../Molecules/MenuList/MenuList.tsx';
 import { UserTypes } from '../../../types/dataTypes.ts';
 import { FC } from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../../store/index.ts';
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderProps {
   user: UserTypes;
 }
 
-const UserMenu = ({user}: {user: UserTypes}) => {
-  return(
-    <div>  
+export const StyledUserMenu = styled.div`
+  ul {
+    padding: 0;
+    list-style: none;
+
+    li {
+      color: ${({ theme }) => theme.colors.white};
+
+      a {
+        color: ${({ theme }) => theme.colors.white};
+        text-decoration: none;
+      }
+    }
+  }
+`;
+
+const UserMenu = ({ user }: { user: UserTypes }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleMockLogout = () => {
+    dispatch(setUser([]));
+    navigate('/');
+  };
+
+  return (
+    <StyledUserMenu>
       <ul>
-        <li><a href={`/user/${user.uuid}`} target='_blank'>My profile</a></li>
-        <li>Log out</li>
+        <li>
+          <a href={`/user/${user.uuid}`} target="_blank">
+            My profile
+          </a>
+        </li>
+        <li onClick={handleMockLogout} style={{ cursor: 'pointer' }}>
+          Log out
+        </li>
       </ul>
-    </div>
-  )
-}
+    </StyledUserMenu>
+  );
+};
 
 const Header: FC<HeaderProps> = (props) => {
   const { user } = props;
