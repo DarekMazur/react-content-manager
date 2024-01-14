723ed3c3ba327cd560085302b8357956d79a08e1
Darek Mazur <dev@darekmazur.it>
2024-01-14 22:38:04 +0100

    Restrict user view for self and admins

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,20 +1,33 @@
 import { useParams } from 'react-router-dom';
-import { useGetUsersQuery } from '../../store';
+import { RootState, useGetUsersQuery } from '../../store';
 import { useEffect, useState } from 'react';
 import { UserTypes } from '../../types/dataTypes';
 import UserForm from '../../components/Organisms/UserForm/UserForm';
+import { useSelector } from 'react-redux';
 
 const UserView = () => {
   const { uuid } = useParams();
-  const { data: users = [] } = useGetUsersQuery();
+  const { data: users = [], isLoading } = useGetUsersQuery();
   const [userData, setUserData] = useState<UserTypes | undefined>(undefined);
+  const currentUser = useSelector<RootState>((state) => state.user);
 
   useEffect(() => {
     setUserData(users.find((user) => user.uuid === uuid) as UserTypes);
   }, [users, uuid]);
 
   return (
-    <>{userData ? <UserForm user={userData} uuid={uuid as string} /> : null}</>
+    <>
+      {userData && !isLoading ? (
+        (currentUser as UserTypes).uuid === uuid ||
+        (currentUser as UserTypes).role.type === 'admin' ? (
+          <UserForm user={userData} uuid={uuid as string} />
+        ) : (
+          "You're not authorised"
+        )
+      ) : (
+        'Loading...'
+      )}
+    </>
   );
 };
 
