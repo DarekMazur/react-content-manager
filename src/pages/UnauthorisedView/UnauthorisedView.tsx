bbff9392c79ab3a04b2bedca83df423dabe481e9
Darek Mazur <dev@darekmazur.it>
2024-02-06 00:30:51 +0100

    Style unauthorised view

--- src/pages/UnauthorisedView/UnauthorisedView.tsx
+++ src/pages/UnauthorisedView/UnauthorisedView.tsx
@@ -1,12 +1,15 @@
 import Heading from '../../components/Atoms/Heading/Heading.tsx';
 import P from '../../components/Atoms/Paragraph/P.tsx';
+import { useTranslation } from 'react-i18next';
+import { StyledUnauthorizedView } from './UnauthirisedView.styles.ts';
 
 const UnauthorisedView = () => {
+  const { t } = useTranslation();
   return (
-    <div>
-      <Heading tag={'h3'}>You are not authorised</Heading>
-      <P>You can't view this page</P>
-    </div>
+    <StyledUnauthorizedView>
+      <Heading tag={'h3'}>{t('unauthorised.header')}</Heading>
+      <P>{t('unauthorised.message')}</P>
+    </StyledUnauthorizedView>
   );
 };
 
