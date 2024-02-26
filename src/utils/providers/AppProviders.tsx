49eabce51c74b775053dc33d21bd6af3e8e96324
Darek Mazur <dev@darekmazur.it>
2024-02-26 22:55:53 +0100

    Add auth provider

--- src/utils/providers/AppProviders.tsx
+++ src/utils/providers/AppProviders.tsx
@@ -8,23 +8,32 @@ import { store } from '../../store';
 import Confirm from '../../components/Organisms/Confirm/Confirm.tsx';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 interface IProvidersProps {
   children: ReactNode;
 }
 const AppProviders: FC<IProvidersProps> = ({ children }) => {
   return (
-    <I18nextProvider i18n={i18next}>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <GlobalStyle />
-            <Confirm />
-            {children}
-          </BrowserRouter>
-        </ThemeProvider>
-      </Provider>
-    </I18nextProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <I18nextProvider i18n={i18next}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <GlobalStyle />
+              <Confirm />
+              {children}
+            </BrowserRouter>
+          </ThemeProvider>
+        </Provider>
+      </I18nextProvider>
+    </Auth0Provider>
   );
 };
 
