e09f927edaaa227d4f6655207cd75027b8e6d61d
Darek Mazur <dev@darekmazur.it>
2024-02-29 16:22:38 +0100

    Fix styles in artticle form

--- src/components/Atoms/Italic/Italic.styles.ts
+++ src/components/Atoms/Italic/Italic.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Italic = styled.span`
+export const Italic = styled.p`
   color: ${({ theme }) => theme.colors.red};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-style: italic;
